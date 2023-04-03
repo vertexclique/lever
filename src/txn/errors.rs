e3b943d4b2fe4eef5e49d84ae350289e0cf204b8
Theo Bulut <vertexclique@gmail.com>
2023-04-03 13:13:38 +0300

    Context-driven Errors

--- src/txn/errors.rs
+++ src/txn/errors.rs
@@ -1,12 +1,16 @@
 use std::result;
 use thiserror::Error;
 
-#[derive(Error, Debug)]
+#[derive(Clone, Error, Debug)]
 pub enum TxnErrorType {
     #[error("Retry mechanism triggered")]
-    Retry = 0,
+    Retry,
     #[error("Abort triggered")]
-    Abort = 1,
+    Abort,
+    #[error("Txn retry with: {0}")]
+    RetryWithContext(String),
+    #[error("Txn abort with: {0}")]
+    AbortWithContext(String)
 }
 
 pub type TxnResult<T> = result::Result<T, TxnErrorType>;
